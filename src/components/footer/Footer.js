d526e9d437866dfc38aa862353defe7d4d4ccfb5
kai <kai@kaiconsulting.co>
2022-06-05 14:54:19 -0600

    fixed header and footer and component page size

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,13 +2,19 @@ import './footer.scss';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function Footer() {
     return (
-        <div className="header">
-        <div className="wrapper">
-            <h1 className="footer">Hello</h1>
-        </div>
+        
+        <div className="footer">
+            <div className="wrapper">
+                
+                {/* <Typography>
+                    <GitHubIcon sx={{ fontSize: 50 }}  />
+                    <LinkedInIcon sx={{ fontSize: 50 }} color="primary" />
+                </Typography> */}
+            </div>
         </div>
     )
 }
